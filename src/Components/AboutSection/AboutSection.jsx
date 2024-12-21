600bba9f4f961a8993fcef8ff7df58ef05198f6d
MeetM2711 <meet@360core.inc>
2024-12-21 18:02:36 +0530

    Update Ui

---
+++ src/Components/AboutSection/AboutSection.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function AboutSection() {
+  return (
+    <section>
+      <div className="container-xxl">
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+          <div className="tag">
+            <img
+              src="https://cdn.prod.website-files.com/60530d64d0c48cbd53eb4a84/6694be527e4af33294bbc2d2_PuzzlePiece.svg"
+              loading="lazy"
+              alt=""
+            />
+            <div className="tag-text">THE SOLUTION</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AboutSection;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,43 +1,19 @@
 import React from "react";
-import Button from "../../Components/BurgerBTN/Button";
+
+import HeroSection from "../../Components/HeroSection/HeroSection";
+import AboutSection from "../../Components/AboutSection/AboutSection";
+import Divider from "../../Components/Divider/Divider";
 // import Toggle from "../../Components/Toggle/Toggle";
 // import HomeLeftHorizontalDivider from "../../Components/Divider/HeroLeftHorizontalDivider";
 // import HomeRightHorizontalDivider from "../../Components/Divider/HeroRightHorizontalDivider";
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center text-[100px] py-24 md:py-32 lg:py-40">
-      <div className="container-xxl">
-        <div className="flex justify-between items-center">
-          
-          <div className="flex flex-col justify-center items-center">
-            <div className="">
-              <h5 className="text-[16px] text-center !m-5">
-                Web & Mobile App Development Company Crealogic
-              </h5>
-            </div>
-            <div className="pb-8 ">
-              <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center font-bold">
-                Your Logical Choice Where Bold Ideas Meet Logical Execution
-              </h1>
-            </div>
-            <div className="">
-              <p className="text-lg md:text-xl text-center text-[#7b7583]">
-                Crealogic is a dynamic force in Web and Mobile App Development.
-                We drive the digital evolution of startups, established brands,
-                and small to medium-sized businesses. We empower our clients to
-                reach their goals by seamlessly blending cutting-edge technology
-                with strategic & logical innovation and shaping a future where
-                possibilities are limitless.
-              </p>
-            </div>
-          <div>
-            <Button>Let's work together</Button>
-          </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <HeroSection />
+      <Divider />
+      <AboutSection />
+    </>
   );
 };
 
