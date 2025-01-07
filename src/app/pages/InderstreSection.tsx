d4e27315d604cb7a5da96cb47aea4d071ad7a4ea
MeetM2711 <meetmangukiya2757@gmail.com>
2025-01-07 14:33:20 +0530

    SVG formate

--- src/app/pages/InderstreSection.tsx
+++ src/app/pages/InderstreSection.tsx
@@ -2,21 +2,21 @@
 import React from "react";
 import HadingTitle from "../Components/HadingTitle";
 import Image from "next/image";
-import Banking from "@/src/app/assets/images/Banking-Finance.webp";
-import Healthcare from "@/src/app/assets/images/Healthcare-Medicine.webp";
-import Travel from "@/src/app/assets/images/Travel-Tourism.webp";
-import Real from "@/src/app/assets/images/Real-Estate.webp";
-import Restaurants from "@/src/app/assets/images/Restaurants.webp";
-import Elearning from "@/src/app/assets/images/E-learning.webp";
-import Ecommerce from "@/src/app/assets/images/e-commerce.webp";
-import Logistics from "@/src/app/assets/images/Logistics-Procurement.webp";
-import Social from "@/src/app/assets/images/Social Network.webp";
-import SaaS from "@/src/app/assets/images/SaaS.webp";
-import Startup from "@/src/app/assets/images/Startup.webp";
-import Advertising from "@/src/app/assets/images/Advertising.webp";
-import Retail from "@/src/app/assets/images/Retail-store.webp";
-import Media from "@/src/app/assets/images/Media-Entertainment.webp";
-import Gaming from "@/src/app/assets/images/Gaming.webp";
+import Banking from "@/src/app/assets/images/Banking-Finance.svg";
+import Healthcare from "@/src/app/assets/images/Healthcare-Medicine.svg";
+import Travel from "@/src/app/assets/images/Travel-Tourism.svg";
+import Real from "@/src/app/assets/images/Real-Estate.svg";
+import Restaurants from "@/src/app/assets/images/Restaurants.svg";
+import Elearning from "@/src/app/assets/images/E-learning.svg";
+import Ecommerce from "@/src/app/assets/images/e-commerce.svg";
+import Logistics from "@/src/app/assets/images/Logistics-Procurement.svg";
+import Social from "@/src/app/assets/images/Social Network.svg";
+import SaaS from "@/src/app/assets/images/SaaS.svg";
+import Startup from "@/src/app/assets/images/Startup.svg";
+import Advertising from "@/src/app/assets/images/Advertising.svg";
+import Retail from "@/src/app/assets/images/Retail-store.svg";
+import Media from "@/src/app/assets/images/Media-Entertainment.svg";
+import Gaming from "@/src/app/assets/images/Gaming.svg";
 
 
 const industries = [
--- src/app/pages/ProcessSection.tsx
+++ src/app/pages/ProcessSection.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import HadingTitle from "../Components/HadingTitle";
 import ProcessBox from "../Components/layout/ProcessBox";
-import Requirement from "../assets/images/Requirement-Analysis.webp";
-import Planning from "../assets/images/Planning-Design.webp";
-import Development from "../assets/images/Development.webp";
-import Testing from "../assets/images/Testing.webp";
-import Deployment from "../assets/images/Deployment.webp";
-import Maintenance from "../assets/images/Maintenance-Support.webp";
+import Requirement from "../assets/images/Requirement-Analysis.svg";
+import Planning from "../assets/images/Planning-Design.svg";
+import Development from "../assets/images/Development.svg";
+import Testing from "../assets/images/Testing.svg";
+import Deployment from "../assets/images/Deployment.svg";
+import Maintenance from "../assets/images/Maintenance-Support.svg";
 
 
 function ProcessSection() {
